b5bee19851bcef62864f867501de3fec765696d7
Stef <stefvanstipdonk@gmail.com>
2023-11-04 20:31:20 +0100

    Created logger

--- outfacingInterfaces/test.cpp
+++ outfacingInterfaces/test.cpp
@@ -4,9 +4,15 @@
 
 #include "test.hpp"
 #include "../src/Wrappers/RenderWrapper.hpp"
+#include "../src/Logger.hpp"
 
 test::test() : renderWrapper(nullptr) {
     renderWrapper = new RenderWrapper();
+
+    Logger::Error("This is an error boi");
+    Logger::Info("This is info boi");
+    Logger::Debug("This is an debug boi");
+//    logger.Error("Fakka");
 }
 
 void test::Run() {
