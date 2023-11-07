5d8125d71e69b64575e015316fa1da7e960e2a8f
jMampaey <82814594+jMampaey@users.noreply.github.com>
2023-11-07 19:34:27 +0100

    Added MovementSystem

--- src/Systems/MovementSystem.cpp
+++ src/Systems/MovementSystem.cpp
@@ -2,7 +2,9 @@
 // Created by jesse on 31/10/2023.
 //
 
+#include <Components/VelocityComponent.hpp>
 #include "MovementSystem.hpp"
+#include "../includes/ComponentStore.hpp"
 
 MovementSystem::MovementSystem() {
 
@@ -13,7 +15,17 @@ MovementSystem::~MovementSystem() {
 }
 
 void MovementSystem::Update(float deltaTime) {
+    auto entities = ComponentStore::GetInstance().getEntitiesWithComponent<VelocityComponent>();
+    for (auto entity : entities) {
+        auto velocityComponent = ComponentStore::GetInstance().getComponent<VelocityComponent>(entity);
+        auto transformComponent = ComponentStore::GetInstance().getComponent<TransformComponent>(entity);
+        transformComponent->position = std::make_unique<Vector2>(
+                *transformComponent->position + velocityComponent->velocity * deltaTime
+        );
+        
+        Logger::Info("Entity: " + std::to_string(entity) + " Position: " + std::to_string(transformComponent->position->getX()) + ", " + std::to_string(transformComponent->position->getY()));
 
+    }
 }
 
 const std::string MovementSystem::GetName() const {
