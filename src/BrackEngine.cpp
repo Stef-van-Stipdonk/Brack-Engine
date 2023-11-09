760bc1a9e0611470364aafe343fc44fe9b98c984
jMampaey <82814594+jMampaey@users.noreply.github.com>
2023-11-09 16:48:54 +0100

    Update BrackEngine.cpp

--- src/BrackEngine.cpp
+++ src/BrackEngine.cpp
@@ -28,7 +28,7 @@ void BrackEngine::Run() {
         FPSSingleton::GetInstance().Start();
         SystemManager::GetInstance().UpdateSystems(GetDeltaTime());
         FPSSingleton::GetInstance().End();
-//        Logger::Info("FPS: " + std::to_string(FPSSingleton::GetInstance().GetFPS()));
+        Logger::Info("FPS: " + std::to_string(FPSSingleton::GetInstance().GetFPS()));
     }
     SystemManager::GetInstance().CleanUp();
 }
