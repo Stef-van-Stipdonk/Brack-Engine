7572e6dbf91132fb65c63fa094091603e2569017
jMampaey <82814594+jMampaey@users.noreply.github.com>
2023-11-20 13:12:23 +0100

    Update GameObjectConverter.hpp

--- src/GameObjectConverter.hpp
+++ src/GameObjectConverter.hpp
@@ -19,6 +19,14 @@ public:
 
     static void AddCamera(Camera *camera);
 
+    static GameObject GetGameObjectByName(const std::string &name);
+
+    static std::vector<GameObject> GetGameObjectsByName(const std::string &name);
+
+    static GameObject GetGameObjectByTag(const std::string &tag);
+
+    static std::vector<GameObject> GetGameObjectsByTag(const std::string &tag);
+    
     static void RemoveGameObject(GameObject &gameObject);
 };
 
