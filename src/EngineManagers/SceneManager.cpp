3fdc16e9c79582017c70302589a3e890657d93f3
jMampaey <82814594+jMampaey@users.noreply.github.com>
2023-11-22 15:04:13 +0100

    Get GameObejcts by tag and name in SceneManager

--- src/EngineManagers/SceneManager.cpp
+++ src/EngineManagers/SceneManager.cpp
@@ -8,7 +8,7 @@
 
 SceneManager SceneManager::instance;
 
-void SceneManager::SetActiveScene(Scene &scene) {
+void SceneManager::setActiveScene(Scene &scene) {
     EntityManager::getInstance().clearAllEntities();
     for (auto camera: scene.GetAllCameras())
         GameObjectConverter::addCamera(camera);
@@ -18,6 +18,22 @@ void SceneManager::SetActiveScene(Scene &scene) {
     }
 }
 
-SceneManager &SceneManager::GetInstance() {
+SceneManager &SceneManager::getInstance() {
     return instance;
+}
+
+std::optional<GameObject> SceneManager::getGameObjectByName(const std::string &name) {
+    return GameObjectConverter::getGameObjectByName(name);
+}
+
+std::vector<GameObject> SceneManager::getGameObjectsByName(const std::string &name) {
+    return GameObjectConverter::getGameObjectsByName(name);
+}
+
+std::optional<GameObject> SceneManager::getGameObjectByTag(const std::string &tag) {
+    return GameObjectConverter::getGameObjectByTag(tag);
+}
+
+std::vector<GameObject> SceneManager::getGameObjectsByTag(const std::string &tag) {
+    return GameObjectConverter::getGameObjectsByTag(tag);
 }
